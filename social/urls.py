197814dc87989fb02cf22bb2e858be77187bc107
Roman27663 <martinezmodz13@gmail.com>
2026-05-29 10:01:42 -0300

    feat: agrega sistema de registro de usuarios

--- social/urls.py
+++ social/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('crear/', views.crear_post, name='crear_post'),
+    path('register/', views.register, name='register'),
+path('register/', views.register, name='register'),
 ]
--- social/views.py
+++ social/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
 
 def home(request):
     posts = Post.objects.all().order_by('-fecha')
@@ -18,3 +20,15 @@ def crear_post(request):
         )
 
         return redirect('home')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
